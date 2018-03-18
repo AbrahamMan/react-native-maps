e248cb381fb1d71aaa8e51cb2c5a66b88777feae
Tung Hoang <hoang.tung@truclearglobal.com>
2018-03-18 22:05:04 +0700

    add movements location

--- App/Containers/Maps.js
+++ App/Containers/Maps.js
@@ -34,6 +34,72 @@ const markers = [
   }
 ]
 
+const vehicleMovements = [
+  {
+    latitude: 21.083347,
+    longitude: 106.09016429
+  },
+  {
+    latitude: 21.08368784,
+    longitude: 106.08984779
+  },
+  {
+    latitude: 21.08391229,
+    longitude: 106.08964628
+  },
+  {
+    latitude: 21.08414186,
+    longitude: 106.08943037
+  },
+  {
+    latitude: 21.08439338,
+    longitude: 106.08919567
+  },
+  {
+    latitude: 21.08468274,
+    longitude: 106.08893081
+  },
+  {
+    latitude: 21.08496053,
+    longitude: 106.08868003
+  },
+  {
+    latitude: 21.08528779,
+    longitude: 106.08838463
+  },
+  {
+    latitude: 21.08566921,
+    longitude: 106.08803405
+  }, {
+    latitude: 21.0859395,
+    longitude: 106.08777253
+  },
+  {
+    latitude: 21.08610092,
+    longitude: 106.08762367
+  },
+  {
+    latitude: 21.08648507,
+    longitude: 106.08726426
+  },
+  {
+    latitude: 21.08690839,
+    longitude: 106.0868669
+  },
+  {
+    latitude: 21.08726651,
+    longitude: 106.08652492
+  },
+  {
+    latitude: 21.0876144,
+    longitude: 106.08620801
+  },
+  {
+    latitude: 21.08840314,
+    longitude: 106.08546269
+  }
+]
+
 export default class MyApp extends React.Component {
   render() {
     const { region } = this.props;
